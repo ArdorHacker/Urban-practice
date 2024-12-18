dbd6fcc9724ec0eda2b7dd035dde9b06ace1b036
ArdorHacker <144014017+ArdorHacker@users.noreply.github.com>
2024-12-18 22:13:20 +0600

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-amount_of_homework = 12
-amount_of_time = 1.5
-name_of_curse = "Python"
-time_for_one_exersize = amount_of_time / amount_of_homework
-
-print(name_of_curse, ", задач: ", amount_of_homework,  ", затрачено часов: ", amount_of_time, ", среднее время выполнения ", time_for_one_exersize,  " часа.")
