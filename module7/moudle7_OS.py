87f4a0e5050cfc64ed8a913cbd19b0546c1075b9
ArdorHacker <144014017+ArdorHacker@users.noreply.github.com>
2025-01-13 19:40:20 +0600

    Update moudle7_OS.py

--- module7/moudle7_OS.py
+++ module7/moudle7_OS.py
@@ -3,9 +3,9 @@ import time
 
 for root, dirs, files in os.walk("module7_1"):
     for file in files:
-        filepath = os.getcwd()
-        filetime = os.path.getmtime(os.path.join(root, file))
+        filepath = os.path.join(root, file)
+        filetime = os.path.getmtime(filepath)
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-        filesize = os.stat(os.path.join(root, file)).st_size
+        filesize = os.stat(filepath).st_size
         parent_dir = root
         print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
